106784d64ab9742a53ede03f50a9f9a7bc32ef05
Ingrid Silva <ingridsilvatec@outlook.com>
2021-06-02 00:29:40 -0300

    cria layout, setta binding

--- app/src/main/java/com/example/expenses/expenses/tracking/MainActivity.kt
+++ app/src/main/java/com/example/expenses/expenses/tracking/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.expenses.expenses.tracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.expenses.expenses.tracking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
