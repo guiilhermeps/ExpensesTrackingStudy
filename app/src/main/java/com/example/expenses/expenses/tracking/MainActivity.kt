28261ff55ce859d629d66f850d2332484b230531
Ingrid Silva <ingridsilvatec@outlook.com>
2021-06-03 00:19:42 -0300

    cria layout com material design

--- app/src/main/java/com/example/expenses/expenses/tracking/MainActivity.kt
+++ app/src/main/java/com/example/expenses/expenses/tracking/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.expenses.expenses.tracking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.expenses.expenses.tracking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
