abed3ac596090819d4200475178afad52e8b073c
Stephanie <stephanieverissimopp@gmail.com>
2021-06-16 22:38:57 -0300

    inicio do constraint

---
+++ app/src/main/java/com/example/expenses/expenses/tracking/Constraint.kt
@@ -0,0 +1,11 @@
+package com.example.expenses.expenses.tracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Constraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
