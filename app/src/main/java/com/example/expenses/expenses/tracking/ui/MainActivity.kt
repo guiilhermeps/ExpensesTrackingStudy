597b37a07b71b8bb4eb43ec86571ba15a309f1cc
Ingrid Silva <ingridsilvatec@outlook.com>
2021-06-17 20:23:15 -0300

    WIP: implementa binding na fragment home

--- app/src/main/java/com/example/expenses/expenses/tracking/ui/MainActivity.kt
+++ app/src/main/java/com/example/expenses/expenses/tracking/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.expenses.expenses.tracking
+package com.example.expenses.expenses.tracking.ui
 
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.expenses.expenses.tracking.ui.fragment.HomeFragment
+import com.example.expenses.expenses.tracking.R
 import com.example.expenses.expenses.tracking.databinding.ActivityMainBinding
-import com.example.expenses.expenses.tracking.databinding.ActivityMainConstraintBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportFragmentManager.beginTransaction()
-            .replace(R.id.frame_holder, LinearFragment()).commit()
+            .replace(R.id.frame_holder, HomeFragment()).commit()
         setUpBottomNavigation()
 
     }
@@ -30,19 +31,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.linear_layout -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frame_holder, LinearFragment()).commit()
-                true
-            }
-            R.id.relative_layout -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frame_holder, RelativeFragment()).commit()
-                true
-            }
             R.id.constraint_layout -> {
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.frame_holder, ConstraintFragment()).commit()
+                    .replace(R.id.frame_holder, HomeFragment(), "HOME_TAG").commit()
                 true
             }
             else -> {
---
+++ app/src/main/java/com/example/expenses/expenses/tracking/ui/fragment/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.expenses.expenses.tracking.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.expenses.expenses.tracking.R
+import com.example.expenses.expenses.tracking.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
