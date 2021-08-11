78305f9712a0098d58f532c127cb15405b44aad2
Stephanie <stephanieverissimopp@gmail.com>
2021-08-11 19:24:53 -0300

    criaçao adapter para o card de adicao

---
+++ app/src/main/java/com/example/expenses/expenses/tracking/fragment/AddCardAdapterFragment.kt
@@ -0,0 +1,40 @@
+package com.example.expenses.expenses.tracking.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.expenses.expenses.tracking.databinding.ItemCardAddBinding
+
+class AddCardAdapterFragment : Fragment() {
+    private lateinit var bindingAddCard: ItemCardAddBinding
+
+    companion object {
+        fun newInstance(): Fragment {
+            return AddCardAdapterFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bindingAddCard = ItemCardAddBinding.inflate(inflater, container, false)
+        return bindingAddCard.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setAddCard()
+    }
+
+    private fun setAddCard() {
+        bindingAddCard.addCard.setOnClickListener {
+            Toast.makeText(context, "Adicionei um Card Uhul", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
