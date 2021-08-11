067c5c34237ee9e698cc8a532aa08944a480e627
Stephanie <stephanieverissimopp@gmail.com>
2021-08-11 19:44:27 -0300

    logica para aparecer o add card quando lista de cartoes  está vazia

---
+++ app/src/main/java/com/example/expenses/expenses/tracking/adapter/CardPagerAdapter.kt
@@ -0,0 +1,29 @@
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.expenses.expenses.tracking.fragment.AddCardAdapterFragment
+import com.example.expenses.expenses.tracking.fragment.CardAdapterFragment
+import com.example.expenses.expenses.tracking.model.Card
+
+class CardPagerAdapter(
+    fragmentManager: FragmentManager,
+    private val cardsList: List<Card>
+) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+    private var listIsEmpty = false
+
+    override fun getCount(): Int {
+        if (cardsList.isEmpty()) {
+            listIsEmpty = true
+            return cardsList.size + 1
+        }
+        return cardsList.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return if(listIsEmpty) {
+            AddCardAdapterFragment.newInstance()
+        } else {
+            CardAdapterFragment.newInstance(cardsList[position])
+        }
+    }
+}
